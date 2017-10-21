cd43eb4ac3626459f3eff14966ea1eebf28fe3d7
EmAchieng <emillyachienggmail.com>
2017-10-21 20:52:03 +0900

    history file

---
+++ history.py
@@ -0,0 +1,122 @@
+lister = ['chris', 'john', 'shithead']
+tuple_lister = ('chris', 'john', 'shithead')
+for i in lister:    print str(i)
+runfile('C:/Users/AA/.spyder-py3/temp.py', wdir='C:/Users/AA/.spyder-py3')
+lines=0
+while lines<50
+
+##---(Sat Oct  7 19:47:41 2017)---
+runfile('C:/Users/AA/.spyder-py3/temp.py', wdir='C:/Users/AA/.spyder-py3')
+
+##---(Sat Oct  7 19:58:35 2017)---
+import Random
+import random
+ans1 = "자! 해보세요!"
+ans2 = "됐네요, 이 사람아"
+ans3 = "뭐라고? 다시 생각해보세요."
+ans4 = "모르니 두려운 것입니다."
+ans5 = "칠문인가요?? 제 정신이 이니군요!"
+!cls
+aa =[]
+!cls
+dic1 = {1:'a', 2: 'b', 3:'c'}
+dic1
+dic2 = {'a':1, 'b':2, 'c':3}
+dic2
+age = 13
+!cls
+runfile('C:/Users/AA/.spyder-py3/temp.py', wdir='C:/Users/AA/.spyder-py3')
+age = 10
+if age > = 10:
+
+##---(Sun Oct  8 01:56:09 2017)---
+students = {'Jack: [2017001, '99'], 'Mary': [2017002, '89'], 'Peter' : [2017003, '87'], 'Jane': [2017004, '80'], 'Janet': [2017005, '75']
+students = {'Jack': [2017001, '99'], 'Mary': [2017002, '89'], 'Peter' : [2017003, '87'], 'Jane': [2017004, '80'], 'Janet': [2017005, '75'] }
+print(students['Janet'])
+print(students['Jane'])
+print(students['Jack'][1])
+print(students)
+runfile('C:/Users/AA/.spyder-py3/temp.py', wdir='C:/Users/AA/.spyder-py3')
+runfile('C:/Users/AA/.spyder-py3/temp.py', wdir='C:/Users/AA/.spyder-py3')
+runfile('C:/Users/AA/.spyder-py3/temp.py', wdir='C:/Users/AA/.spyder-py3')
+
+##---(Sun Oct  8 17:47:21 2017)---
+runfile('C:/Users/AA/.spyder-py3/temp.py', wdir='C:/Users/AA/.spyder-py3')
+3
+50
+30
+runfile('C:/Users/AA/.spyder-py3/temp.py', wdir='C:/Users/AA/.spyder-py3')
+60
+
+##---(Tue Oct 10 17:29:38 2017)---
+runfile('C:/Users/AA/.spyder-py3/conditions_python.py', wdir='C:/Users/AA/.spyder-py3')
+runfile('C:/Users/AA/.spyder-py3/conditions_python.py', wdir='C:/Users/AA/.spyder-py3')
+
+##---(Wed Oct 11 09:29:59 2017)---
+n = input("integer")
+n = int(n)
+runfile('C:/Users/AA/.spyder-py3/conditions_python.py', wdir='C:/Users/AA/.spyder-py3')
+runfile('C:/Users/AA/tuples.py', wdir='C:/Users/AA')
+runfile('C:/Users/AA/tuples.py', wdir='C:/Users/AA')
+1 + 2
+"1" + "2"
+"tup2[1:2]"
+tup2[1:2]
+print (tup2[1:2], tup2[1:2])
+tup2[1:2]
+runfile('C:/Users/AA/tuples.py', wdir='C:/Users/AA')
+t1 = 1, 2, 3
+t1
+type(t1)
+x, y, z = t1
+x
+y
+z
+t1 = 1
+type(t1)
+t1 = (1)
+type(t1)
+t1 =( 1,)
+type(t1)
+t1 =1, 2, 3
+li = list(li)
+t1 ={1, 2, 3}
+li 
+li= list(t1)
+li=[1,2]
+type(li)
+type(t1)
+d1 = {'a': 1, 'b': 2]
+d1 = {'a': 1, 'b': 2}
+li = list(d1)
+type(d1)
+d1 = dict(l2)
+l2 = {['a': 2], ['b', 3]}
+runfile('C:/Users/AA/.spyder-py3/conditions_python.py', wdir='C:/Users/AA/.spyder-py3')
+runfile('C:/Users/AA/dict.py', wdir='C:/Users/AA')
+runfile('C:/Users/AA/.spyder-py3/conditions_python.py', wdir='C:/Users/AA/.spyder-py3')
+runfile('C:/Users/AA/dict.py', wdir='C:/Users/AA')
+runfile('C:/Users/AA/dict.py', wdir='C:/Users/AA')
+runfile('C:/Users/AA/aDict.py', wdir='C:/Users/AA')
+
+##---(Wed Oct 11 20:52:43 2017)---
+runfile('C:/Users/AA/aDict.py', wdir='C:/Users/AA')
+runfile('C:/Users/AA/dict.py', wdir='C:/Users/AA')
+runfile('C:/Users/AA/aDict.py', wdir='C:/Users/AA')
+
+##---(Wed Oct 18 09:19:11 2017)---
+runfile('C:/Users/AA/students1.py', wdir='C:/Users/AA')
+runfile('C:/Users/AA/airfare.py', wdir='C:/Users/AA')
+runfile('C:/Users/AA/airfare.py', wdir='C:/Users/AA')
+runfile('C:/Users/AA/students1.py', wdir='C:/Users/AA')
+runfile('C:/Users/AA/students1.py', wdir='C:/Users/AA')
+x,y  =(3, 4)
+x -y if x>y else y-x
+runfile('C:/Users/AA/students1.py', wdir='C:/Users/AA')
+
+##---(Sat Oct 21 19:28:24 2017)---
+runfile('C:/Users/AA/.spyder-py3/temp.py', wdir='C:/Users/AA/.spyder-py3')
+runfile('C:/Users/AA/aDict.py', wdir='C:/Users/AA')
+runfile('C:/Users/AA/aDict.py', wdir='C:/Users/AA')
+runfile('C:/Users/AA/.spyder-py3/aDict.py', wdir='C:/Users/AA/.spyder-py3')
+runfile('C:/Users/AA/.spyder-py3/dict.py', wdir='C:/Users/AA/.spyder-py3')
