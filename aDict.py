87591009c74275056ffbc1dac83b44b0c7ed88a4
EmAchieng <emillyachienggmail.com>
2017-10-21 20:45:54 +0900

    a dictionary

---
+++ aDict.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 12:07:58 2017
+
+@author: AA
+"""
+#students dictionary
+Student1 = {"name":"minyong", "grade":"A"}
+Student2 = {"name": "Grace", "grade" : "B+"}
+Student3 ={"name" : "Mary", "grade" : "A"}
+
+D = {"201735958":Student1, "201700005" : Student2, "201715531" : Student3}
+
+
+#output
+print(D)
+print(D['201700005'])
+
+
