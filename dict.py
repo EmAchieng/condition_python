b7a6716b3b0f0897c07807bcf3d0363e88424e2f
EmAchieng <emillyachienggmail.com>
2017-10-21 20:49:17 +0900

    dictionary

---
+++ dict.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 11:49:43 2017
+
+@author: AA
+"""
+
+
+
+#Korean-English Dictionary
+#define the dictionary
+aDict ={}
+
+#add words to the dictionary
+aDict = {'mother': ['어머니'], 'father' : ['아버지'], 'house' : ['집'], 'bread' : ['빵'], 'country': ['국적']}
+
+#print the dictionary
+print(aDict)
