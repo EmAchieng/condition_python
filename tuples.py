7d4922dc77dd87f3b1e8c5e2a3ed24ff7f0e0001
EmAchieng <emillyachienggmail.com>
2017-10-21 20:57:49 +0900

    tuples

---
+++ tuples.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 10:19:03 2017
+
+@author: AA
+"""
+
+#define the dictinary
+ages={}
+
+#add  names to the dictiionary
+ages['sue'] =18
+ages['peter'] =20
+ages['andrew'] = 19
+ages['karren'] = 17
+
+#use the has_key function
+print(ages)
+
+tup1 = ('chemistry', 'physics', 'maths');
+tup2 = ( 1, 2, 3);
+tup3 = ( "a", "b", "c")
+
+print (tup1[1:2], tup2[1:2])
+tup2[1:2]
+
+a =  [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print (a[::2])
+
+a=[1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(a[::2]) = 10, 20, 30, 40, 50, 60
+
+print(a)
+
+a=[1, 2, 3, 4, 5]
+print (a[3:0:-1])
+
